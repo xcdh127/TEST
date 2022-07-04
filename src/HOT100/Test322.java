e963c9a4d6835d46f5059dd284066c7b23cd2450
sxc <s2839846578@163.com>
2022-07-04 09:00:33 +0800

    第一次提交

---
+++ src/HOT100/Test322.java
@@ -0,0 +1,67 @@
+package HOT100;
+
+import java.util.Arrays;
+
+/*给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+你可以认为每种硬币的数量是无限的。
+示例 1：
+输入：coins = [1, 2, 5], amount = 11
+输出：3
+解释：11 = 5 + 5 + 1
+示例 2：
+输入：coins = [2], amount = 3
+输出：-1
+示例 3：
+输入：coins = [1], amount = 0
+输出：0
+提示：
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+*/
+public class Test322 {
+    public int coinChange(int[] coins, int amount) {
+        int n=coins.length;
+        int[][] dp=new int[n+1][amount+1];
+        //为所有的位置赋值为amount+1
+        for (int i = 0; i <=n ; i++) {
+            Arrays.fill(dp[i],amount+1);
+        }
+        //用若干硬币填充满容量为0的背包的最少硬币数量为0
+        for (int i = 0; i <=n; i++) {
+            dp[i][0]=0;
+        }
+        for (int i = 1; i <=n ; i++) {
+            int num=coins[i-1];
+            for (int j = 1; j <=amount ; j++) {
+                for (int k = 0; j-num*k>=0 ; k++) {
+                    dp[i][j]=Math.min(dp[i][j],dp[i-1][j-num*k]+k);
+                }
+            }
+        }
+        return dp[n][amount]>amount?-1:dp[n][amount];
+    }
+}
+/*class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n=coins.length;
+        int[][] dp=new int[n+1][amount+1];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i],amount+1);
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            int num=coins[i-1];
+            for(int j=1;j<=amount;j++){
+                for(int k=0;j-num*k>=0;k++){
+                    dp[i][j]=Math.min(dp[i][j],dp[i-1][j-num*k]+k);
+                }
+            }
+        }
+        return dp[n][amount]>amount?-1:dp[n][amount];
+
+    }
+}*/
