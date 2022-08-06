63ff7c96d123bfeaa9fcce7e71f5a6fc60617fee
sxc <s2839846578@163.com>
2022-08-06 20:50:12 +0800

    20220806

---
+++ src/CodeDream/TestOffer05.java
@@ -0,0 +1,22 @@
+package CodeDream;
+
+/*请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+示例 1：
+输入：s = "We are happy."
+输出："We%20are%20happy."
+限制：
+0 <= s 的长度 <= 10000
+*/
+public class TestOffer05 {
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                sb.append(s.charAt(i));
+            } else {
+                sb.append("%20");
+            }
+        }
+        return sb.toString();
+    }
+}
