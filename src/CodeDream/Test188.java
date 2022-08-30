74b17554afb4441e89ad29ed9a7599d781de1594
sxc <s2839846578@163.com>
2022-08-30 19:57:06 +0800

    20220830

--- src/CodeDream/Test188.java
+++ src/CodeDream/Test188.java
@@ -19,7 +19,27 @@ package CodeDream;
 */
 public class Test188 {
     public int maxProfit(int k, int[] prices) {
-        return 0;
+        if (prices.length == 0) {
+            return 0;
+        }
+        if (k == 0) {
+            return 0;
+        }
+        int[] dp = new int[2 * k];
+        for (int i = 0; i < dp.length / 2; i++) {
+            dp[i * 2] = -prices[0];
+        }
+        for (int i = 1; i <= prices.length; i++) {
+            //买入股票
+            dp[0] = Math.max(dp[0], -prices[i - 1]);
+            //卖出股票
+            dp[1] = Math.max(dp[1], dp[0] + prices[i - 1]);
+            for (int j = 2; j < dp.length; j += 2) {
+                dp[j] = Math.max(dp[j], dp[j - 1] - prices[i - 1]);
+                dp[j + 1] = Math.max(dp[j + 1], dp[j] + prices[i - 1]);
+            }
+        }
+        return dp[dp.length - 1];
     }
 }
 
