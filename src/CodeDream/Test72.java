95d2fba5dd851693e6d851fb39d271acdf32b652
sxc <s2839846578@163.com>
2022-09-02 15:23:01 +0800

    20220902

---
+++ src/CodeDream/Test72.java
@@ -0,0 +1,83 @@
+package CodeDream;
+
+/*给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数  。
+你可以对一个单词进行如下三种操作：
+插入一个字符
+删除一个字符
+替换一个字符
+示例 1：
+输入：word1 = "horse", word2 = "ros"
+输出：3
+解释：
+horse -> rorse (将 'h' 替换为 'r')
+rorse -> rose (删除 'r')
+rose -> ros (删除 'e')
+示例 2：
+输入：word1 = "intention", word2 = "execution"
+输出：5
+解释：
+intention -> inention (删除 't')
+inention -> enention (将 'i' 替换为 'e')
+enention -> exention (将 'n' 替换为 'x')
+exention -> exection (将 'n' 替换为 'c')
+exection -> execution (插入 'u')
+提示：
+0 <= word1.length, word2.length <= 500
+word1 和 word2 由小写英文字母组成
+*/
+public class Test72 {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        //字符串2为空时
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        //字符串1为空时
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                //当单词1的i位置的字符与单词2的j位置的字符相等时，插入单词1，插入单词2，和替换末尾字符取最小值，操作次数+1
+                //此时两个单词的字符相等不需要替换
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i + 1][j + 1] = Math.min(dp[i + 1][j], Math.min(dp[i][j + 1], dp[i][j] - 1)) + 1;
+                }
+                //当单词1的i位置的字符与单词2的j位置的字符相等时，插入单词1，插入单词2，和替换末尾字符取最小值，操作次数+1
+                else {
+                    dp[i + 1][j + 1] = Math.min(dp[i + 1][j], Math.min(dp[i][j + 1], dp[i][j])) + 1;
+                }
+            }
+        }
+        //返回使得单词1和单词2相等的最小操作次数
+        return dp[m][n];
+    }
+}
+
+class Solution25 {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        //字符串word1与空字符串word2转换，需要的最少操作次数为字符串word1的长度
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        //字符串word2与空字符串word1转换，需要的最少操作次数为字符串word2的长度
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i + 1][j + 1] = Math.min(Math.min(dp[i + 1][j], dp[i][j + 1]), dp[i][j] - 1) + 1;
+                } else {
+                    dp[i + 1][j + 1] = Math.min(Math.min(dp[i + 1][j], dp[i][j + 1]), dp[i][j]) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
